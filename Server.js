db4a69ccee8606f286f53dc5a45cb000651cbd87
nitinrajputind <nitinrajput971625@gmail.com>
2023-09-06 20:02:57 +0530

    Routes is Added

--- Server.js
+++ Server.js
@@ -5,7 +5,13 @@ const express = require('express');
 const app = express();
 
 // rest Api 
+
 app.get("/", (req,res)=>{
+    res.send("WelCome To the Express !! Server is Runing Now");
+});
+
+
+app.get("/main", (req,res)=>{
     res.send({
         "what is Express?" : "Express.js is a small framework that works on top of Node.js web server functionality to simplify its APIs and add helpful new features. It makes it easier to organize your application’s functionality with middleware and routing. It adds helpful utilities to Node.js HTTP objects and facilitates the rendering of dynamic HTTP objects."
     });
